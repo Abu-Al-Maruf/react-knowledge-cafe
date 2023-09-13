fe6a278a8f5143fdca7552839171cedb8e1d4389
Abu Al Maruf <mdmaruf55667711@gmail.com>
2023-09-14 00:17:41 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-yellow-400 text-xl">
+        Knowledge Cafe
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
